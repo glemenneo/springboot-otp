e6d20d43bf3e5ef245559140cfbfa2b45a49a148
Glemen Neo <glemenneo@gmail.com>
2024-01-22 13:43:06 +0800

    Remove redundant auth provider

--- src/main/kotlin/com/example/ipwhitelist/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/ipwhitelist/config/SecurityConfig.kt
@@ -1,9 +1,7 @@
 package com.example.ipwhitelist.config
 
-import com.example.ipwhitelist.repository.UserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -11,10 +9,8 @@ import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-@EnableWebSecurity
-class SecurityConfig(
-    private val authenticationProvider: CustomAuthenticationProvider
-) {
+@EnableWebSecurity()
+class SecurityConfig {
     @Bean
     fun filterChain(
         http: HttpSecurity,
@@ -33,7 +29,6 @@ class SecurityConfig(
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
-            .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
     }
